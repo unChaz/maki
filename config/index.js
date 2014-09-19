07cb1e4d5f379ead3833bfea428bf49c97496fc5
Chaz Ferguson <chaz.ferguson@gmail.com>
2014-09-19 01:56:15 -0400

    config

--- config/index.js
+++ config/index.js
@@ -41,8 +41,8 @@ module.exports = {
     }
   },
   app: {
-    bitpayEnv: "chaz.bp:8088",
-    apiKey: "wbwu4GngrK5471gUA5Fkf8kLEYCPBebDEYhXT0Gn7c",
-    goal: 15000
+    bitpayEnv: "bitpay.com",
+    apiKey: "BRrn9LuQqcv5iNZOkwSGXrARc0DlLcdxNZLqJaap0cQ",
+    goal: 1000
   }
 };
